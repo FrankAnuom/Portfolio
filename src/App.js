1710538b1eae070033c3f96595f7c07c8d2b9ad5
Anuom Chika Frank <anuomochika@gmail.com>
2024-01-19 01:58:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import NavBar from './Pages/Home/NavBar';
-import Home from './Pages/Home/Homescreen';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Pages/Home/Navbar";
+import Home from "./Pages/Home/Homescreen";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <div>
-          <NavBar/>
+          <Navbar />
           <Routes>
-            <Route path='/' element={<Home/>}>
-            </Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={<div>404 Not Found</div>}></Route>
           </Routes>
         </div>
       </Router>
